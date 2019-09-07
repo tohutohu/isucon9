5158f5e3bdf43b097dcb98e47f8e1fbb77540e32
tohutohu <tohu.soy@gmail.com>
2019-09-07 07:58:29 +0000

    bench

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -586,7 +586,7 @@ func postInitialize(w http.ResponseWriter, r *http.Request) {
 
 	res := resInitialize{
 		// キャンペーン実施時には還元率の設定を返す。詳しくはマニュアルを参照のこと。
-		Campaign: 4,
+		Campaign: 6,
 		// 実装言語を返す
 		Language: "Go",
 	}
