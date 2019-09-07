ce618bd0c360050c76c50eda355ac5fd7605573d
tohutohu <tohu.soy@gmail.com>
2019-09-07 04:22:54 +0000

    bench

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -918,7 +918,6 @@ func getTransactions(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	tx := dbx.MustBegin()
 	// items := []Item{}
 	var rows *sqlx.Rows
 	if itemID > 0 && createdAt > 0 {
@@ -1039,20 +1038,17 @@ ORDER BY created_at DESC, items.id DESC LIMIT ?`,
 		if err != nil {
 			log.Print(err)
 			outputErrorMsg(w, http.StatusInternalServerError, "db error")
-			tx.Rollback()
 			return
 		}
 
-		seller, err := getUserSimpleByID(tx, item.SellerID)
+		seller, err := getUserSimpleByID(dbx, item.SellerID)
 		if err != nil {
 			outputErrorMsg(w, http.StatusNotFound, "seller not found")
-			tx.Rollback()
 			return
 		}
-		category, err := getCategoryByID(tx, item.CategoryID)
+		category, err := getCategoryByID(dbx, item.CategoryID)
 		if err != nil {
 			outputErrorMsg(w, http.StatusNotFound, "category not found")
-			tx.Rollback()
 			return
 		}
 
@@ -1076,10 +1072,9 @@ ORDER BY created_at DESC, items.id DESC LIMIT ?`,
 		}
 
 		if item.BuyerID != 0 {
-			buyer, err := getUserSimpleByID(tx, item.BuyerID)
+			buyer, err := getUserSimpleByID(dbx, item.BuyerID)
 			if err != nil {
 				outputErrorMsg(w, http.StatusNotFound, "buyer not found")
-				tx.Rollback()
 				return
 			}
 			itemDetail.BuyerID = item.BuyerID
@@ -1107,94 +1102,6 @@ ORDER BY created_at DESC, items.id DESC LIMIT ?`,
 		}
 		itemDetails = append(itemDetails, itemDetail)
 	}
-
-	// itemDetails := []ItemDetail{}
-	// for _, item := range items {
-	// 	seller, err := getUserSimpleByID(tx, item.SellerID)
-	// 	if err != nil {
-	// 		outputErrorMsg(w, http.StatusNotFound, "seller not found")
-	// 		tx.Rollback()
-	// 		return
-	// 	}
-	// 	category, err := getCategoryByID(tx, item.CategoryID)
-	// 	if err != nil {
-	// 		outputErrorMsg(w, http.StatusNotFound, "category not found")
-	// 		tx.Rollback()
-	// 		return
-	// 	}
-
-	// 	itemDetail := ItemDetail{
-	// 		ID:       item.ID,
-	// 		SellerID: item.SellerID,
-	// 		Seller:   &seller,
-	// 		// BuyerID
-	// 		// Buyer
-	// 		Status:      item.Status,
-	// 		Name:        item.Name,
-	// 		Price:       item.Price,
-	// 		Description: item.Description,
-	// 		ImageURL:    getImageURL(item.ImageName),
-	// 		CategoryID:  item.CategoryID,
-	// 		// TransactionEvidenceID
-	// 		// TransactionEvidenceStatus
-	// 		// ShippingStatus
-	// 		Category:  &category,
-	// 		CreatedAt: item.CreatedAt.Unix(),
-	// 	}
-
-	// 	if item.BuyerID != 0 {
-	// 		buyer, err := getUserSimpleByID(tx, item.BuyerID)
-	// 		if err != nil {
-	// 			outputErrorMsg(w, http.StatusNotFound, "buyer not found")
-	// 			tx.Rollback()
-	// 			return
-	// 		}
-	// 		itemDetail.BuyerID = item.BuyerID
-	// 		itemDetail.Buyer = &buyer
-	// 	}
-
-	// 	transactionEvidence := TransactionEvidence{}
-	// 	err = tx.ExecRow(&transactionEvidence, "SELECT * FROM `transaction_evidences` WHERE `item_id` = ?", item.ID)
-	// 	if err != nil && err != sql.ErrNoRows {
-	// 		// It's able to ignore ErrNoRows
-	// 		log.Print(err)
-	// 		outputErrorMsg(w, http.StatusInternalServerError, "db error")
-	// 		tx.Rollback()
-	// 		return
-	// 	}
-
-	// 	if transactionEvidence.ID > 0 {
-	// 		shipping := Shipping{}
-	// 		err = tx.Get(&shipping, "SELECT * FROM `shippings` WHERE `transaction_evidence_id` = ?", transactionEvidence.ID)
-	// 		if err == sql.ErrNoRows {
-	// 			outputErrorMsg(w, http.StatusNotFound, "shipping not found")
-	// 			tx.Rollback()
-	// 			return
-	// 		}
-	// 		if err != nil {
-	// 			log.Print(err)
-	// 			outputErrorMsg(w, http.StatusInternalServerError, "db error")
-	// 			tx.Rollback()
-	// 			return
-	// 		}
-	// 		ssr, err := APIShipmentStatus(getShipmentServiceURL(), &APIShipmentStatusReq{
-	// 			ReserveID: shipping.ReserveID,
-	// 		})
-	// 		if err != nil {
-	// 			log.Print(err)
-	// 			outputErrorMsg(w, http.StatusInternalServerError, "failed to request to shipment service")
-	// 			tx.Rollback()
-	// 			return
-	// 		}
-
-	// 		itemDetail.TransactionEvidenceID = transactionEvidence.ID
-	// 		itemDetail.TransactionEvidenceStatus = transactionEvidence.Status
-	// 		itemDetail.ShippingStatus = ssr.Status
-	// 	}
-
-	// 	itemDetails = append(itemDetails, itemDetail)
-	// }
-	tx.Commit()
 
 	hasNext := false
 	if len(itemDetails) > TransactionsPerPage {
